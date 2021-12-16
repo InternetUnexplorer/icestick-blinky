d3c7d86e1eeb4394498a05b54dfef0dbb2bf487f
InternetUnexplorer <internetunexplorer@gmail.com>
2021-12-15 19:03:28 -0800

    Tweak things a bit and add a reset pin

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 OUTPUT_DIR ?= target
-SCALA_SRCS ?= $(shell find src -name "*.scala")
+SCALA_SRCS := $(shell find src -name "*.scala")
 
 .PHONY: default prog clean
 
@@ -9,10 +9,10 @@ $(OUTPUT_DIR)/Blinky.v: $(SCALA_SRCS)
 	sbt "runMain Blinky --target-dir $(OUTPUT_DIR)"
 
 $(OUTPUT_DIR)/Blinky.json: $(OUTPUT_DIR)/Blinky.v
-	yosys -q -p "synth_ice40 -top Blinky -json $@" $<
+	yosys -p "synth_ice40 -top Blinky -json $@" $<
 
-$(OUTPUT_DIR)/Blinky.asc: $(OUTPUT_DIR)/Blinky.json icestick.pcf
-	nextpnr-ice40 -q --json $< --asc $@ --pcf icestick.pcf --hx1k --package tq144
+$(OUTPUT_DIR)/Blinky.asc: $(OUTPUT_DIR)/Blinky.json pinmap.pcf
+	nextpnr-ice40 --json $< --asc $@ --pcf pinmap.pcf --hx1k --package tq144
 
 $(OUTPUT_DIR)/Blinky.bin: $(OUTPUT_DIR)/Blinky.asc
 	icepack $< $@
--- src/main/scala/Blinky.scala
+++ src/main/scala/Blinky.scala
@@ -1,13 +1,9 @@
 import chisel3._
+import chisel3.stage.ChiselStage
 import chisel3.util.Counter
 
-// We don't need a reset, so we make `Blinky` a `RawModule` and define the clock
-// explicitly to avoid an implicit reset.
-
-class Blinky extends RawModule {
+class Blinky extends Module {
   val io = IO(new Bundle {
-    val clock = Input(Clock())
-
     val led1 = Output(Bool())
     val led2 = Output(Bool())
     val led3 = Output(Bool())
@@ -15,20 +11,18 @@ class Blinky extends RawModule {
     val led5 = Output(Bool())
   })
 
-  withClockAndReset(io.clock, false.B) {
-    val CLOCK_HZ = 12_000_000 // 12 MHz
-    val (counter, _) = Counter(0 until CLOCK_HZ)
+  val CLOCK_HZ = 12_000_000 // 12 MHz
+  val (counter, _) = Counter(0 until CLOCK_HZ)
 
-    io.led1 := false.B
-    io.led2 := false.B
-    io.led3 := false.B
-    io.led4 := false.B
-    io.led5 := (counter > (CLOCK_HZ / 2).U)
-  }
+  io.led1 := false.B
+  io.led2 := false.B
+  io.led3 := false.B
+  io.led4 := false.B
+  io.led5 := (counter > (CLOCK_HZ / 2).U)
 }
 
 // This part generates the Verilog.
 
 object Blinky extends App {
-  (new chisel3.stage.ChiselStage).emitVerilog(new Blinky, args)
+  (new ChiselStage).emitVerilog(new Blinky, args)
 }
