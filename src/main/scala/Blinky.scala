5c68ac7dae5f4aba3d3182a37de5cb8b9bc34287
InternetUnexplorer <internetunexplorer@gmail.com>
2021-12-27 17:14:39 -0800

    Remove `io_` prefix from led pins

--- src/main/scala/Blinky.scala
+++ src/main/scala/Blinky.scala
@@ -3,26 +3,19 @@ import chisel3.stage.ChiselStage
 import chisel3.util.Counter
 
 class Blinky extends Module {
-  val io = IO(new Bundle {
-    val led1 = Output(Bool())
-    val led2 = Output(Bool())
-    val led3 = Output(Bool())
-    val led4 = Output(Bool())
-    val led5 = Output(Bool())
-  })
+  val led1, led2, led3, led4, led5 = IO(Output(Bool()))
 
   val CLOCK_HZ = 12_000_000 // 12 MHz
   val (counter, _) = Counter(0 until CLOCK_HZ)
 
-  io.led1 := false.B
-  io.led2 := false.B
-  io.led3 := false.B
-  io.led4 := false.B
-  io.led5 := (counter > (CLOCK_HZ / 2).U)
+  led1 := false.B
+  led2 := false.B
+  led3 := false.B
+  led4 := false.B
+  led5 := (counter > (CLOCK_HZ / 2).U)
 }
 
 // This part generates the Verilog.
-
 object Blinky extends App {
   (new ChiselStage).emitVerilog(new Blinky, args)
 }
